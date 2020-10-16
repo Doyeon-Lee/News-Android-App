9fa53acf340ee01513284ca38d2104c4c8b7f4fd
Doyeon-Lee <dyl1229@naver.com>
2020-10-16 21:14:46 +0900

    ADD TOGGLE BUTTON AT PW FRAGMENT

--- app/src/main/java/com/example/myapplication/PasswordFragment.java
+++ app/src/main/java/com/example/myapplication/PasswordFragment.java
@@ -1,27 +1,27 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class PasswordFragment extends Fragment {
-    EditText EditText_original_pw, EditText_new_pw, EditText_new_pw2;
-    TextView TextView_warning, TextView_warning2;
-    Button Button_submit;
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText EditText_original_pw, EditText_new_pw, EditText_new_pw2;
+    private TextView TextView_warning, TextView_warning2, TextView_warning3;
+    private ToggleButton ToggleButton_pw1, ToggleButton_pw2, ToggleButton_pw3;
+    private Button Button_submit;
 
     public PasswordFragment() {
         // Required empty public constructor
@@ -31,8 +31,6 @@ public class PasswordFragment extends Fragment {
     public static PasswordFragment newInstance(String param1, String param2) {
         PasswordFragment fragment = new PasswordFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -41,8 +39,6 @@ public class PasswordFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -57,6 +53,40 @@ public class PasswordFragment extends Fragment {
         TextView_warning = v.findViewById(R.id.TextView_warning);
         TextView_warning2 = v.findViewById(R.id.TextView_warning2);
         Button_submit = v.findViewById(R.id.Button_submit);
+        ToggleButton_pw1 = v.findViewById(R.id.ToggleButton_pw1);
+        ToggleButton_pw2 = v.findViewById(R.id.ToggleButton_pw2);
+        ToggleButton_pw3 = v.findViewById(R.id.ToggleButton_pw3);
+
+        ToggleButton_pw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    EditText_original_pw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                else
+                    EditText_original_pw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+        });
+
+        ToggleButton_pw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    EditText_new_pw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                else
+                    EditText_new_pw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+        });
+
+        ToggleButton_pw3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    EditText_new_pw2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                else
+                    EditText_new_pw2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+        });
+
 
         Button_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,8 +96,9 @@ public class PasswordFragment extends Fragment {
                 String new_pw2 = EditText_new_pw2.getText().toString();
                 boolean flag;
 
-                String password = ((MainActivity)getContext()).user.getPassword();
-                //String password = ((MainActivity)MainActivity.context).user.password;
+                //String password = ((MainActivity)getActivity()).user.getPassword();
+                //User user = ((MainActivity)getActivity()).user;
+                String password = "";
 
                 if(original_pw.equals(password)){
                     flag = true;
@@ -86,7 +117,7 @@ public class PasswordFragment extends Fragment {
                 }
 
                 if(flag){ //비밀번호 변경 성공
-
+                    Toast.makeText(getActivity(), "SUCCESS", Toast.LENGTH_SHORT).show();
                 }
             }
         });
